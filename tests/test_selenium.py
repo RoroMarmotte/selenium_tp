7b54f7846f6ef9d9c1ccff85889607f82017c714
RoroMarmotte <rohanteeluck937@gmail.com>
2026-02-11 12:40:49 +0100

    fix

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -23,8 +23,7 @@ class TestCalculator:
             chrome_options.add_argument('--disable-dev-shm-usage')
             chrome_options.add_argument('--disable-gpu')
             chrome_options.add_argument('--window-size=1920,1080')
-        service = Service(ChromeDriverManager().install())
-        driver = webdriver.Chrome(service=service, options=chrome_options)
+        driver = webdriver.Chrome(options=chrome_options)
         driver.implicitly_wait(10)
         yield driver
         driver.quit()
